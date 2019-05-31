4807c87666089ecb312ff1f693339bfe54fb0150
nyxrobotics <takaaki.numai01@g.softbank.co.jp>
2019-05-31 19:29:21 +0900

    短い単語も拾うように修正

--- speach2text.py
+++ speach2text.py
@@ -3,23 +3,32 @@
 #coding: UTF-8
 import os,subprocess
 import io
-
+import subprocess, signal
 
 class juliusSpeechToText:
     def __init__(self):
+        self.pkill_julius()
         self.startListening()
     def __del__(self):
         self.stopListening()
-
+        self.pkill_julius()
     def startListening(self):
         # cmd = 'bash ./lib/dictation-kit/run-linux-dnn-minimal.sh'
         cmd = 'cd ~/lib/julius_libs/dictation-kit/;bash run-linux-dnn.sh'
         self.julius_process = subprocess.Popen(cmd,stdin=subprocess.PIPE, stdout=subprocess.PIPE,shell=True)
-        # print('debug_1\n')
+        print('julius:started\n')
 
     def stopListening(self):
-        # print('debug_3\n')
+        print('julius:successfully finished\n')
         self.julius_process.kill()
+    def pkill_julius(self):
+        p = subprocess.Popen(['pgrep', '-l' , 'julius'], stdout=subprocess.PIPE)
+        out, err = p.communicate()
+        for line in out.splitlines():        
+            line = bytes.decode(line)
+            pid = int(line.split(None, 1)[0])
+            os.kill(pid, signal.SIGKILL)
+
 
     def textStreaming(self):
         # print('debug_2\n')
@@ -29,9 +38,16 @@ class juliusSpeechToText:
                 out_string = line.rstrip('\n')
                 # print(out_string)
                 find_text_flag = out_string.find('sentence1:  ')
+                find_short_text_flag = out_string.find('<input rejected by short input>')
                 if find_text_flag != -1:
                     text_string = out_string.lstrip('sentence1:  ')
                     if text_string != '。':
+                        # None
+                        print('text:'+ text_string)
+                if find_short_text_flag != -1:
+                    text_string = out_string.rstrip('<input rejected by short input>')
+                    text_string = text_string.lstrip('pass1_best:  ')
+                    if text_string != '':
                         # None
                         print('text:'+ text_string)
 
